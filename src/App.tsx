b01ed3717adb69816906b528865f080bacdf09d4
deepakcodez <deepak2603dk@gmail.com>
2025-02-20 15:12:24 +0530

    proj setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+function App() {
+  1;
+  return (
+    <>
+      <h1 className="bg-neutral-900  h-screen w-full">
+        <div className="flex justify-center items-center h-full">
+        <h1 className="text-[14rem] font-semibold text-white/50">OM Vault</h1>
+        </div>
+      </h1>
+    </>
+  );
+}
+
+export default App;
